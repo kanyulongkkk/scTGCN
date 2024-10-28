36d11a9c331ef4aaf2d6c2719f9924ad57ac048f
kanyulongkkk <44249859+kanyulongkkk@users.noreply.github.com>
2024-10-28 16:45:33 +0800

    Update model_regress.py

--- util/model_regress.py
+++ util/model_regress.py
@@ -1,54 +1,44 @@
 import torch
 import torch.nn as nn
-from torch_geometric.nn import GATv2Conv
+from torch_geometric.nn import GCNConv
+from torch_geometric.nn import SAGEConv, global_mean_pool
 from torch.nn import Linear
+class GraphMix(nn.Module):
+    def __init__(self, num_of_class):
+        super(GraphMix, self).__init__()
+        self.gcn = GCNConv(64, 32)
+        self.out = Linear(32, num_of_class)
+
+    def forward(self, x, edge_index):
+        h = self.gcn(x, edge_index)
+        embedding = torch.relu(h)
+        z = self.out(embedding)
+        return z
 
 class Net_encoder(nn.Module):
-    def __init__(self, input_size):
+    def __init__(self, input_size, hidden_size=64):
         super(Net_encoder, self).__init__()
         self.input_size = input_size
+        self.k = 64
+        self.f = 64
+
         self.encoder = nn.Sequential(
-            nn.Linear(self.input_size, 64),
-            nn.ReLU()
+            nn.Linear(self.input_size, hidden_size)
         )
 
     def forward(self, data):
         data = data.float().view(-1, self.input_size)
         embedding = self.encoder(data)
+
         return embedding
 
-class GNN(nn.Module):
-    def __init__(self, num_of_class):
-        super(GNN, self).__init__()
-
-        self.init_conv = GATv2Conv(64, 8,heads=2)
-        self.num_heads = 16
-        self.head_convs = nn.ModuleList([GATv2Conv(16, 1024) for _ in range(self.num_heads)])
-        self.gat = GATv2Conv(1024*self.num_heads, 1024, heads=self.num_heads)  # Uncommented this line
-        self.out = Linear(1024 * self.num_heads, num_of_class)  
-
-    def forward(self, x, edge_index):
-        # Initial convolution to extract features
-        x = self.init_conv(x, edge_index)
-
-        # Applying head convolutions
-        head_outputs = [head_conv(x, edge_index) for head_conv in self.head_convs]
-
-        # Concatenating the outputs from all head convolutions
-        h = torch.cat(head_outputs, dim=-1)  # Concatenate along the feature dimension
-
-        # Final GAT layer
-        gat_output = self.gat(h, edge_index)
-
-        # Final output layer
-        z = self.out(gat_output)
-        return z
 class Net_cell(nn.Module):
     def __init__(self, num_of_class):
         super(Net_cell, self).__init__()
-        self.cell = GNN(num_of_class)
+        self.cell = GraphMix(num_of_class)
 
     def forward(self, embedding, edge_index):
         cell_prediction = self.cell(embedding, edge_index)
+
         return cell_prediction
  
